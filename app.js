f333241af697ce7880a7ac98bd21f23cebb71d51
zachary webb <zachsavn@gmail.com>
2022-08-13 22:33:22 -0400

    created exercise_redo branch

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
 
-
-const plus = (num) => {
-   return (plusNumber) => (plusNumber += num);
-};
-  let  plus15 = plus(15);
-
-console.log(plus15(10))
 

6ae53d30dc6ddda2ce04b16d10b134053b391c53
zachary webb <zachsavn@gmail.com>
2022-08-13 23:21:40 -0400

    Finished exercise 2

--- app.js
+++ app.js
@@ -8,3 +8,34 @@ function plus(number) {
 
 let plus15 = plus(15);
 console.log(plus15(10));
+
+//Exercise 2 Section forEach
+
+let users = [
+  {
+    name: "Frodo",
+    age: 50,
+    score: 85,
+    isActive: false,
+  },
+  {
+    name: "Sam",
+    age:38,
+    score:94,
+    isActive:true
+  },
+  {
+    name:"Merry",
+    age:36,
+    score: 82,
+    isActive:true
+  },
+  {
+    name: "Pippin",
+    age: 26,
+    score: 77,
+    isActive: false
+  }
+];
+//iterates over array and grabs only name
+users.forEach(user => console.log(user.name));
